5b4ffd18ba8b6a1b75bf25bb0aea632ec2d2b7cc
DevMateusmac <99038035+DevMateusmac@users.noreply.github.com>
2022-04-21 21:09:38 -0300

    inicio do exercício

---
+++ exercicio_09/exercitando_funcoes.js
@@ -0,0 +1,8 @@
+let nameShip = prompt(`Insira aqui o nome da nave!`)
+let velocity = 0
+let newVelocity 
+
+function menu() {
+    prompt(`Menu de bordo: \n1- Acelerar a nave em 5km/s. \n2- Desacelerar a nave em 5km/s. \n3- Imprimir dados de bordo. \n4- Encerrar viagem e sair do programa.`)
+}
+menu()
