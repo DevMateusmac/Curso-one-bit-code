19b7c083de61e91840eb922b36610ae10fdbbbae
DevMateusmac <99038035+DevMateusmac@users.noreply.github.com>
2022-04-27 16:29:20 -0300

    Update teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -1,11 +1,1 @@
 // somente para fazer os exercícios das aulas conceituais dos tópicos de JS, os exercícios estarão em pastas separadas.
-
-function reduceVelocity(){
-    let velocity = 150
-    console.log(`Estamos a ${velocity}km/s`)
-    for(vel = velocity; vel >= 0; vel -=20){
-        console.log(`Diminuido a velocidade em ${vel}km/s.`)
-    }
-
-}
-reduceVelocity()
