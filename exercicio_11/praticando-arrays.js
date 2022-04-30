4df534b1b959989b9f50eb901300739ad5ba7ec4
DevMateusmac <99038035+DevMateusmac@users.noreply.github.com>
2022-04-30 20:18:53 -0300

    início do exercício

---
+++ exercicio_11/praticando-arrays.js
@@ -0,0 +1,11 @@
+const hitchedSpaceships = [
+    ["Fenix", 8, true],
+    ["Golias", 10, true],
+    ["Helmet", 5, false],
+    ["Elemental", 3, true],
+    ["Darwin", 15, false]
+  ]
+
+  let test = hitchedSpaceships.forEach(function(arrayname){
+      console.log(arrayname)
+  })
