b1b1bee260ef6971ecbaf6a028e3c43a85f807d8
DevMateusmac <99038035+DevMateusmac@users.noreply.github.com>
2022-05-11 20:41:58 -0300

    Update objetos.js

--- exercicio_12/objetos.js
+++ exercicio_12/objetos.js
@@ -1,7 +1,14 @@
 let spaceship = {
     name: prompt(`Qual é o nome da nave?`),
     type: prompt(`Qual é o tipo da sua nave? \n1-Nave de infantaria. \n2-Nave de Reconhecimento. \n3-Nave de Intendência. \n4-Nave de Material bélico `),
-    maxVelocity: prompt(`Qual é a velocidade máxima que deseja configurar para a nave em km/s?`)
+    velocity: 0,
+    maxVelocity: prompt(`Qual é a velocidade máxima que deseja configurar para a nave em km/s?`),
+    menu: function(){
+        let option 
+       option = prompt(`O que deseja fazer agora? \n1 - Acelerar a nave. \n2 - Parar a nave.` )
+                   
+        return option
+    }
 }
 
 switch(spaceship.type){
@@ -20,4 +27,31 @@ switch(spaceship.type){
     default:
         spaceship.type = "Nave comum"
 }
+
+
+spaceship.speedUp = function (velocity, maxVelocity){
+    spaceship.otherVelocity = prompt(`Quanto você quer acelerar a nave?`)
+    
+    if(otherVelocity > spaceship.maxVelocity){
+        otherVelocity = maxVelocity
+       
+        alert(`Velocidade escolhida ultrapassa o valor máximo escolhido para a nave!`) 
+        velocity += spaceship.otherVelocity
+        alert(`A velocidade atual da nave é ${spaceship.velocity}`)
+    }else {
+        velocity += spaceship.otherVelocity
+        alert(`Velocidade aumentada para ${spaceship.velocity}.`)
+    }
+}
+
+do{
+    chosenOption = spaceship.menu()
+    switch(chosenOption){
+        case "1":
+            spaceship.speedUp()
+            break
+        case "2":
+            alert(`Parando a nave! \nNome da nave: ${spaceship.name}. \nTipo da nave: ${spaceship.type} \nVelocidade alcançada: ${spaceship.velocity}.`)
+    }
+} while(chosenOption != "2")
 
