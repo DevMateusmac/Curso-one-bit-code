4f0328d01755ad1f9aba5ef11407ea299c18b603
DevMateusmac <99038035+DevMateusmac@users.noreply.github.com>
2022-05-20 17:08:24 -0300

    att

--- teste/teste.js
+++ teste/teste.js
@@ -1,1 +1,8 @@
 // somente para fazer os exercícios das aulas conceituais dos tópicos de JS, os exercícios estarão em pastas separadas.
+
+
+let limite = 10
+
+for(cont = 1; cont > limite; cont++){
+    console.log(limite)
+}
