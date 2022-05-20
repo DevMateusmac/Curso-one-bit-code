a66690691985e4c2adae903db6eba1490290a78e
DevMateusmac <99038035+DevMateusmac@users.noreply.github.com>
2022-05-20 17:31:41 -0300

    Update teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -1,8 +1,2 @@
 // somente para fazer os exercícios das aulas conceituais dos tópicos de JS, os exercícios estarão em pastas separadas.
 
-
-let limite = 10
-
-for(cont = 1; cont > limite; cont++){
-    console.log(limite)
-}
