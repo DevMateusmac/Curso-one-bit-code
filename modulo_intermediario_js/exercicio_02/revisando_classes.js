8d1709bfcc1400f953fef638f509fb808560b7e7
Mateus Maciulevicius <dev.mateusmac@gmail.com>
2022-07-06 18:42:40 -0300

    Finalização e correção
    Aqui finalizei o exercício, e corrigi colocando a resolução do professor em uma pasta separada para possíveis consultas futuramente.
    A minha versão ficou certa e funcionando também.

--- modulo_intermediario_js/exercicio_02/revisando_classes.js
+++ modulo_intermediario_js/exercicio_02/revisando_classes.js
@@ -45,6 +45,15 @@ class TransportSpaceship extends Spaceship{
     }
 }
 
+
+function menu1(){
+    let option 
+    while(option != "1" && option != "2" && option != "3" && option != "4"){
+        option = prompt("O que deseja fazer? \n1- cadastrar nave. \n2- Acelerar a nave. \n3- Trocar a nave. \n4- Imprimir e sair.")
+    }
+    return option
+}
+
 function cadastroNaveDeBatalha(){
     let spaceshipName = prompt("Qual é o nome da sua nave de batalha?")
     let crewQuantity = prompt("Quantos tripulantes tem na sua nave?")
@@ -52,7 +61,6 @@ function cadastroNaveDeBatalha(){
 
     let otherBattleSpaceship = new BattleSpaceship(spaceshipName, crewQuantity, numberOfGuns)
         return otherBattleSpaceship
-    
 }
 
 function cadastroNaveDeTransporte(){
@@ -62,15 +70,6 @@ function cadastroNaveDeTransporte(){
 
     let otherTransportSpaceship = new TransportSpaceship(spaceshipName, crewQuantity, numberOfSits)
         return otherTransportSpaceship
-}
-
-
-function menu1(){
-    let option 
-    while(option != "1" && option != "2" && option != "3" && option != "4"){
-        option = prompt("O que deseja fazer? \n1- cadastrar nave. \n2- Acelerar a nave. \n3- Trocar a nave. \n4- Imprimir e sair.")
-    }
-    return option
 }
 
 
@@ -88,29 +87,47 @@ function cadastroNovaNave(){
     return newShip
 }
 
+function boardPanelAndExit(spaceshipsAdded){
+    spaceshipsAdded.forEach(function(currentspaceship){
+        alert(`Nome da nave: ${currentspaceship.name} \nTripulantes: ${currentspaceship.crew} \nVelocidade atual: ${currentspaceship.currentVelocity}`)
+    })
+    alert("Encerrando painel de bordo, até a próxima!")
+}
+
+let spaceshipsAdded = []
+
+
 function changeShip(){
     alert("Trocando espaçonave!")
     let changeSpaceship = cadastroNovaNave()
     return changeSpaceship
 }
 
-let spaceshipsAdded = []
-
+let chosenOption
 do{
-    let chosenOption = menu1()
+    chosenOption = menu1()
     switch(chosenOption){
         case "1":
+            if(spaceshipsAdded[0] != undefined){
+                alert("Você já cadastrou uma nave! considere mudar a nave para refazer o processo")
+                break
+            }                
             let spaceshipToAdd = cadastroNovaNave()
                 spaceshipsAdded.push(spaceshipToAdd)
                 break
         case "2":
-            //acelerar nave
+                let acceleration = prompt("Quantos km/s você quer acelerar a nave?")
+                spaceshipsAdded[0].speedUp(acceleration)
+                alert(`Velocidade atualizada para ${spaceshipsAdded[0].currentVelocity} com a redução de segurança!`)
+
                 break
         case "3":
-            //trocar nave
+            spaceshipsAdded = []
+                let changingShip = changeShip()
+                spaceshipsAdded.push(changingShip)
                 break
         case "4":
-            //mostrar nave e sair
+            boardPanelAndExit(spaceshipsAdded)
                 break
         default: alert("Algo de errado interrompeu o funcionamento do sistema, tente novamente mais tarde!")
                 break
