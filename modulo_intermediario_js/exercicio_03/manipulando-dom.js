99981c7faef9381519dc1a57e14766b52fdb45c3
Mateus Maciulevicius <dev.mateusmac@gmail.com>
2022-07-12 23:05:28 -0300

    JS e HTML
    inicio do exercício de prática do DOM e módulo de HTML

---
+++ modulo_intermediario_js/exercicio_03/manipulando-dom.js
@@ -0,0 +1,15 @@
+
+function showAddres(){
+    let addres = document.getElementsByName("addres")
+    addres.forEach((addres) => {
+        console.log(addres.value)
+    })
+
+  
+
+}
+
+let p = document.createElement("p")
+document.body.appendChild(p)
+//p.innerText = "TO APRENDENDO ESSA CACETA"
+//console.log(p)
